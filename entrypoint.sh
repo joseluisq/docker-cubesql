e75790bd3b32a5fa9e1fa950f4a1b5986d630d18
Jose Quintana <joseluisquintana20@gmail.com>
2022-01-17 17:34:22 +0100

    fix: settings map

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,20 +24,20 @@ else
 
         if [ -n "$CUBESQL_SSL_ONLY" ] && [ "$CUBESQL_SSL_ONLY" = "true" ]; then X_OPTS="$X_OPTS -q"; fi
 
-        if [ -n "$CUBESQL_SSL_CERTIFICATE" ]; then X_OPTS="$X_OPTS -m=$CUBESQL_SSL_CERTIFICATE"; fi
+        if [ -n "$CUBESQL_SSL_CERTIFICATE" ]; then X_OPTS="$X_OPTS -m $CUBESQL_SSL_CERTIFICATE"; fi
 
-        if [ -n "$CUBESQL_CA_ROOT_CERTIFICATE" ]; then X_OPTS="$X_OPTS -a=$CUBESQL_CA_ROOT_CERTIFICATE"; fi
+        if [ -n "$CUBESQL_CA_ROOT_CERTIFICATE" ]; then X_OPTS="$X_OPTS -a $CUBESQL_CA_ROOT_CERTIFICATE"; fi
 
         if [ -n "$CUBESQL_MAX_SHARED_DATABASES" ]; then X_OPTS="$X_OPTS -o=$CUBESQL_MAX_SHARED_DATABASES"; fi
 
         if [ -z "$CUBESQL_DEBUG_MODE" ]; then export CUBESQL_DEBUG_MODE=$X_DEBUG_MODE; fi
-        X_OPTS="$X_OPTS -g=$CUBESQL_DEBUG_MODE"
+        X_OPTS="$X_OPTS -g $CUBESQL_DEBUG_MODE"
 
         if [ -z "$CUBESQL_LOG_FORMAT" ]; then export CUBESQL_LOG_FORMAT=$X_LOG_FORMAT; fi
-        X_OPTS="$X_OPTS -f=$CUBESQL_LOG_FORMAT"
+        X_OPTS="$X_OPTS -f $CUBESQL_LOG_FORMAT"
 
         if [ -z "$CUBESQL_LOG_VERBOSITY" ]; then export CUBESQL_LOG_VERBOSITY=$X_LOG_VERBOSITY; fi
-        X_OPTS="$X_OPTS -v=$CUBESQL_LOG_VERBOSITY"
+        X_OPTS="$X_OPTS -v $CUBESQL_LOG_VERBOSITY"
 
         # cubeSQL custom options
         if [ -n "$CUBESQL_OPTS" ]; then X_OPTS="$X_OPTS $CUBESQL_OPTS"; fi
